bc79aa526048ca4304aa0e1a5685f6ab1e545175
許浩鳴 <minghsu0107@gmail.com>
2020-08-13 15:45:45 +0800

    add

--- System/my_async4.py
+++ System/my_async4.py
@@ -12,29 +12,37 @@ API_ENTPOINT = os.getenv('API_ENTPOINT')
 
 
 def blockingRequest():
-    payload = {}
-    mylist = ['hello', 'world']
-    mydict = {'hello': 'world', 'world': 'hello'}
-    payload['data'] = mylist
-    payload['other'] = mydict
-    headers = {'Content-Type': 'application/json'}
+    success = False
+    tries = 0
+    predictions = []
+    while not success:
+        tries += 1
+        try:
+            payload = {}
+            mylist = ['hello', 'world']
+            mydict = {'hello': 'world', 'world': 'hello'}
+            payload['data'] = mylist
+            payload['other'] = mydict
+            headers = {'Content-Type': 'application/json'}
+            resp = requests.post(API_ENTPOINT, json=payload, headers=headers)
+            resp.raise_for_status()
+            predictions = resp.json()['predictions']
+            success = True
+        except requests.HTTPError as errh:
+            logging.error("An Http Error occurred:" + repr(errh))
+        except requests.exceptions.ConnectionError as errc:
+            logging.error(
+                "An Error Connecting to the API occurred:" + repr(errc))
+        except requests.exceptions.Timeout as errt:
+            logging.error("A Timeout Error occurred:" + repr(errt))
+        except requests.exceptions.RequestException as err:
+            logging.error("An Unknown Error occurred" + repr(err))
+        finally:
+            if tries >= 3:
+                logging.error('tries exceeded:%s ', tries)
+                return []
 
-    try:
-        resp = requests.post(API_ENTPOINT, json=payload, headers=headers)
-        resp.raise_for_status()
-        return resp.json()['predictions']
-    except requests.HTTPError as errh:
-        print("An Http Error occurred:" + repr(errh))
-        raise
-    except requests.exceptions.ConnectionError as errc:
-        print("An Error Connecting to the API occurred:" + repr(errc))
-        raise
-    except requests.exceptions.Timeout as errt:
-        print("A Timeout Error occurred:" + repr(errt))
-        raise
-    except requests.exceptions.RequestException as err:
-        print("An Unknown Error occurred" + repr(err))
-        raise
+    return predictions
 
 
 async def main():
