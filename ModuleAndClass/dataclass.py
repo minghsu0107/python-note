baa90bf3da017b26806b46a1d4c83e5cbc000570
Hao-Ming, Hsu <50090692+minghsu0107@users.noreply.github.com>
2022-07-15 15:36:29 +0800

    Create dataclass.py

---
+++ ModuleAndClass/dataclass.py
@@ -0,0 +1,20 @@
+from dataclasses import dataclass, field
+
+@dataclass
+class Product:
+    name: str
+    price: int
+    qty: int
+    amount: int = field(init=False)
+
+    def __post_init__(self):
+        self.amount = self.price * self.qty
+        
+    def __eq__(self, other):
+        return self.price == other.price
+
+
+itemA = Product(name="itemA", price=100, qty=10)
+itemB = Product(name="itemB", price=100, qty=15)
+print(itemA)
+print(itemA == itemB)
